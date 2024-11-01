1bfdaa7b19e8fd2162051b121e1ddf9e9c66a297
pbxr250 <pbxr250@yahoo.com>
2024-11-01 18:05:01 +0300

    stuff10

--- src/utils/webapp.ts
+++ src/utils/webapp.ts
@@ -15,7 +15,8 @@ userdata.user = windowExt.Telegram?.WebApp?.initDataUnsafe?.user?.id ?? ''
 userdata.username = windowExt.Telegram?.WebApp?.initDataUnsafe?.user?.username ?? ''
 userdata.query_id = windowExt.Telegram?.WebApp?.initDataUnsafe?.user?.query_id ?? ''
 
-console.log('Debug log from webapp.ts, initdata:' + userdata)
+console.log(`Debug log from webapp.ts, initstring: ${windowExt.Telegram?.WebApp?.initData}`)
+console.log(`Debug log from webapp.ts, userdata: ${userdata.user} ${userdata.username} ${userdata.query_id}`)
 //console.log('Debug log from webapp.ts' + windowExt.Telegram.WebApp.initDataUnsafe)
 
 export const tgWebApp = windowExt.Telegram.WebApp
