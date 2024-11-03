aaa0bba224f27d7d74dcfe6da7380ce124dcb03b
pbxr250 <pbxr250@yahoo.com>
2024-11-03 18:21:44 +0300

    stuff12

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { tgWebApp, userdata, mode, MODE } from "./utils/webapp"
+
+const root = createRoot(document.getElementById('root')!)
+
+try {
+    if (import.meta.env.PROD) 
+        if ( userdata.query_id === '' || mode === MODE.IncorrectMode )
+            throw new Error('Mini App should be started from Telegram');
+
+    root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+    )
+} catch ( e ) {
+    root.render(<h1>Mini App should be started from Telegram</h1>)
+}
--- src/utils/webapp.ts
+++ src/utils/webapp.ts
@@ -11,12 +11,27 @@ export const userdata = {
     query_id: ''
 }
 
+export enum MODE { Customer = 'Customer', Vendor = 'Vendor', IncorrectMode = 'IncorrectMode'}
+
 userdata.user = windowExt.Telegram?.WebApp?.initDataUnsafe?.user?.id ?? ''
 userdata.username = windowExt.Telegram?.WebApp?.initDataUnsafe?.user?.username ?? ''
 userdata.query_id = windowExt.Telegram?.WebApp?.initDataUnsafe?.query_id ?? ''
 
-console.log(`Debug log from webapp.ts, initstring: ${windowExt.Telegram?.WebApp?.initData}`)
-console.log(`Debug log from webapp.ts, userdata: ${userdata.user} ${userdata.username} ${userdata.query_id}`)
-//console.log('Debug log from webapp.ts' + windowExt.Telegram.WebApp.initDataUnsafe)
+// href="/booker/startapp?mode=client" values = {client, vendor}
+export const mode = (( value: string ) => {
+    if ( value === 'customer' )
+        return MODE.Customer
+    if ( value === 'vendor' )
+        return MODE.Vendor
+    return MODE.IncorrectMode
+})( windowExt.location.search.split('=')[1])
+
+if (import.meta.env.DEV) {
+    console.log(`Debug log from webapp.ts,href: ${windowExt.location.href} `)
+    console.log(`Debug log from webapp.ts, MODE: ${mode} `)
+    console.log(`Debug log from webapp.ts, initstring: ${windowExt.Telegram?.WebApp?.initData}`)
+    console.log(`Debug log from webapp.ts, userdata: ${userdata.user} ${userdata.username} ${userdata.query_id}`)
+    //console.log('Debug log from webapp.ts' + windowExt.Telegram.WebApp.initDataUnsafe)
+}
 
 export const tgWebApp = windowExt.Telegram.WebApp
